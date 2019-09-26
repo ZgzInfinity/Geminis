f947a0bdac9ad0b6420da5bda76089e524107cf1
vpec <741294@unizar.es>
2019-09-26 18:10:18 +0200

    Point struct
    Co-authored-by: ZgzInfinity <737215@unizar.es>

--- include/Point.h
+++ include/Point.h
@@ -1,6 +1,9 @@
 #ifndef POINT_H
 #define POINT_H
 
-
+struct Point {
+    float c[4];
+    Point(float x, float y, float z);
+};
 
 #endif /* POINT_H */
--- src/Point.cpp
+++ src/Point.cpp
@@ -1,1 +1,8 @@
 #include "../include/Point.h"
+
+Point::Point(float x, float y, float z) {
+    c[0] = x;
+    c[1] = y;
+    c[2] = z;
+    c[3] = 1;
+}
