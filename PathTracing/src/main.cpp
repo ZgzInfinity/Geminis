c2f0aaf3cdbe5d5f4d9026f1d9c8b3c5cd9fc1f3
vpec <741294@unizar.es>
2019-11-08 22:36:56 +0100

    getting pixel dimensions
    Co-authored-by: ZgzInfinity <rubenrodriguez1998@hotmail.com>

--- PathTracing/src/main.cpp
+++ PathTracing/src/main.cpp
@@ -12,7 +12,11 @@
 
 #include <iostream>
 #include <cstring>
+#include <list>
+#include <iomanip>
 #include "../include/Matrix4.h"
+#include "../include/Plane.h"
+#include "../include/Sphere.h"
 
 using namespace std;
 
@@ -28,12 +32,39 @@ int main(int argc, char* argv[]){
         int raysPerPixel = atoi(argv[1]);
         cout << "Number of rays per pixel " << raysPerPixel << endl;
 
-        Direction d_i = Direction(1, 0, 0);
+        Direction d_i = Direction(0, 0, -1);
         Direction d_j = Direction(0, 1, 0);
-        Direction d_k = Direction(0, 0, 1);
+        Direction d_k = Direction(1, 0, 0);
         Point origin = Point(0, 0, 0);
 
-        Matrix4 Camara = Matrix4::changeBase(d_i, d_j, d_k, origin);
+        Matrix4 camera = Matrix4::changeBase(d_i, d_j, d_k, origin);
+
+        // Image resolution
+        float width = 100;
+        float height = 100;
+
+        // Projection plane
+        Direction leftPP = Direction(0, 0, -1);
+        Direction upPP = Direction(0, 1, 0);
+
+        list <Sphere> sphereList;
+        list <Plane> planeList;
+
+        float pixelDimension = mod(leftPP) / (width / 2.f);
+
+        if(pixelDimension != mod(upPP) / (height / 2.f)){
+            // Incorrect
+            cerr << "Bad resolution and Project plane configuration" << endl;
+        }
+        else {
+            // Correct
+            cout << "The pixel is square with dimension " << pixelDimension << endl;
+        }
+
+
+
+
+
     }
 
     return 0;
