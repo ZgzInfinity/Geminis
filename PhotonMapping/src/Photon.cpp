7fcd55463ff8164cb7a5cad839974e18fb35ad6c
DESKTOP-6V36O5F\Gord <737215@unizar.es>
2020-01-08 17:14:56 +0100

    Photonmapper preprocess
    Co-Authored-By: Víctor Peñasco <victorpenasco1@gmail.com>

--- PhotonMapping/src/Photon.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/Photon.h"
-
-Photon::Photon(){}
-
-Photon::Photon(RGB _flux, Point _location, Direction _sourceDir){
-    flux = _flux;
-    location = _location;
-    sourceDir = _sourceDir;
-}
